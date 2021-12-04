7eaac864ebc070862131c1f277981d64048079e9
sirishaasapu <asapusirisha@gmail.com>
2021-12-04 13:32:16 +0530

    get directory,file path

--- global.js
+++ global.js
@@ -1,15 +1,21 @@
 //In Browser window is used to see the global objects
 
-console.log(global);
+// console.log(global);
 
 //setTimeout
-setTimeout(()=>{
-    console.log("hello sirisha"),
-    clearInterval(time);
-},3000);
+// setTimeout(()=>{
+//     console.log("hello sirisha"),
+//     clearInterval(time);
+// },3000);
 
 //setInterval
-let time=setInterval(()=>{
-    console.log("Time Intervals");
-},1000);
+// let time=setInterval(()=>{
+//     console.log("Time Intervals");
+// },1000);
 
+//get directory 
+console.log(__dirname);
+
+//get file path
+
+console.log(__filename);
