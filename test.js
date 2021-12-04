5de74b300dcb598a4f27efecd28cde2439c8723f
sirishaasapu <asapusirisha@gmail.com>
2021-12-04 12:43:28 +0530

    greet method in js

--- test.js
+++ test.js
@@ -1,2 +1,7 @@
-const name="sirisha";
-console.log(name);
+// const name="sirisha";
+// console.log(name);
+
+const greet=(fname,lname)=>{
+    console.log(`Hello ${fname} ${lname}`);
+}
+greet("sirisha","Asapu");
