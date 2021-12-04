ab747b868fe6d9540fb27e837791799df676728b
sirishaasapu <asapusirisha@gmail.com>
2021-12-04 20:58:02 +0530

    serving json data

--- serve.js
+++ serve.js
@@ -1,17 +1,17 @@
 let http=require('http');
-let fileSys=require('fs');
+// let fileSys=require('fs');
 
 let server=http.createServer(function(req,res){
     console.log(req.url);
     res.writeHead(200,{'content-Type':'text/html'});
-    let myReadStream=fileSys.createReadStream(__dirname + '/index.html','utf8');
-    myReadStream.pipe(res);
+    // let myReadStream=fileSys.createReadStream(__dirname + '/index.html','utf8');
+    // myReadStream.pipe(res);
     //res.end("hello sirisha");
-    // let myObj={
-    //     name:'sirisha asapu',
-    //     age:21,
-    // };
-    // res.end(JSON.stringify(myObj));
+    let myObj={
+        name:'sirisha asapu',
+        age:21,
+    };
+    res.end(JSON.stringify(myObj));
    
 });
 server.listen(3000,'127.0.0.1');
