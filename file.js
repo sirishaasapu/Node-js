69c97d5e8c353c61efb5f19bc44e114e377fa86e
sirishaasapu <asapusirisha@gmail.com>
2021-12-04 14:59:13 +0530

    writing file

--- file.js
+++ file.js
@@ -2,6 +2,12 @@
 
 //creating directory
 let fileSys=require('fs');
-fileSys.mkdir('./Doc',()=>{
-    console.log("directory created")
+// fileSys.mkdir('./Doc',()=>{
+//     console.log("directory created")
+// })
+
+
+//writing files
+fileSys.writeFile('./Doc/bolg1.txt',"hello sirisha",()=>{
+    console.log("file created")
 })
