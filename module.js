bbfa7388b99c7e40245d80cc4a949ccc72f9efe5
sirishaasapu <asapusirisha@gmail.com>
2021-12-04 14:31:31 +0530

    About operating system

--- module.js
+++ module.js
@@ -1,7 +1,10 @@
-let sData=require("./data");
+let {studata,stuId}=require("./data");
  
-console.log(sData);
-console.log(sData.studata);
-console.log(sData.stuId);
+// console.log(sData);
+console.log(studata,stuId);
+// console.log(sData.stuId);
+
+const os=require('os');
+console.log(os.hostname(),os.loadavg(),os.platform())
 
 
